8cf88a51cd78d6a28ffcee6e2133969044983898
TheBeachmaster <otienoken@outlook.com>
2017-11-01 00:19:25 +0300

    Cleanups

--- ruby/at.sms/sms.rb
+++ ruby/at.sms/sms.rb
@@ -1,15 +1,14 @@
 # Include the helper gateway class
 require 'AfricasTalkingGateway'
 
-username = "KennedyOtieno";
-# env = "sandbox";
-apikey   = "9f072129e0b34163eb8460a42f537bbb1564f50f8ca069fa779e27c64171f4e1";
+username = "me";
+apikey   = "key";
 
 # Specify the numbers that you want to send to in a comma-separated list
 # Please ensure you include the country code (+254 for Kenya)
-to      = "+254724587654";
+to      = "+NUM,+NUM";
 
-message = "Rubyyyyyy...."
+message = "Sample SMS Code written in Ruby,by you know who 😉...."
 
 gateway = AfricasTalkingGateway.new(username, apikey)
 
