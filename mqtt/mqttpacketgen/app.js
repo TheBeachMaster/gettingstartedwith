8d87443d327302e609e471b112969b0818209fbf
Thebeachmaster <otienoken@outlook.com>
2018-02-15 14:41:36 +0300

    Add comments to code for clarity

--- mqtt/mqttpacketgen/app.js
+++ mqtt/mqttpacketgen/app.js
@@ -3,9 +3,12 @@ let mqtt = require('mqtt-packet');
 const connect = {
     cmd: 'connect',
     protocolId: 'MQTT',
+    dup: false,
     protocolVersion: 4,
     clean: true,
     retain: false,
+    topic: null,
+    payload: null,
     qos: 0,
     keepalive: 15,
     clientId: 'client_test1',
@@ -13,4 +16,35 @@ const connect = {
     password: new Buffer('password')
 }
 
+var object = {
+    cmd: 'publish',
+    messageId: 42,
+    qos: 1,
+    dup: false,
+    topic: 'willTopic',
+    payload: new Buffer('test'),
+    retain: false
+}
+
+const publishPacket = {
+    cmd: 'publish',
+    qos: 1,
+    dup: false,
+    topic: 'willTopic',
+    retain: false,
+    payload: 'mqttpayload'
+}
+
+const connAck = {
+    cmd: 'connack',
+    returnCode: 0,
+    sessionPresent: false
+}
+
+const pubAck = {
+    cmd: 'puback',
+    messageId: 4660,
+    qos: 1,
+    topic: 'topic'
+}
 console.log(mqtt.generate(connect))
--- python/mqtt/app.py
+++ python/mqtt/app.py
@@ -8,29 +8,42 @@ client = mqtt.Client("paho_mqtt_client")
 username = "paho"
 pahopassword = "pahopassword"
 
+# fired when ckeint succesfully subscribes to a topic
 def on_subscribe(client, obj, mid, granted_qos):
     print("Subscribed:" + str(mid) + " " + str(granted_qos))
-    
+
+# fired when a client successfully connects to the server    
 def on_connect(client, userdata, flags, rc):
     print("Connected with code: " + str(rc))
 
+# fired when a client disconnects from the server
 def on_diconnect(client, userdata, rc):
     print("Successfully disconected from the broker")
 
+# fired when a client receives a message from the subscribed topic
 def on_message(client, userdata, message):
     timer.sleep(1)
     print("Received a message from the broker: ", str(message.payload.decode("utf-8")))
 
+# create a bunch of random values and convert the values(int) to string . Stringify
 def randomize_values(a, b):
     sensor_value = randint(a, b)
     sensor_payload = str(sensor_value)
     return sensor_payload
+
+# supply authentication details to a connnecting client
 client.username_pw_set(username, password=pahopassword)
+
+# callbacks to be triggered when stuff happens : Event loops
 client.on_message = on_message
 client.on_subscribe = on_subscribe
 client.on_connect = on_connect
 client.on_diconnect = on_diconnect
+
+# connction details (broker url, broker port, keepalive<=60)
 client.connect(broker, brokerPort, 60)
+
+# strange behavior where the publsihing occurs faster than the on_connect event is fired first
 timer.sleep(5)
 counter = 0
 while (counter < 30):
